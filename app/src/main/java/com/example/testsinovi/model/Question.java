0084c199243999d565badaa23c43341b5cb40688
13 DIMI Laptop <valikhanovabdumalik@gmail.com>
2022-06-22 23:01:30 +0500

    IShlashini tekshirish

--- app/src/main/java/com/example/testsinovi/model/Question.java
+++ app/src/main/java/com/example/testsinovi/model/Question.java
@@ -1,13 +1,15 @@
 package com.example.testsinovi.model;
 
 import androidx.room.ColumnInfo;
+import androidx.room.Entity;
 import androidx.room.PrimaryKey;
 
 import java.sql.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
-
+@Entity
 public class Question {
     @PrimaryKey(autoGenerate = true)
     int uid;
@@ -30,6 +32,64 @@ public class Question {
         this.answerB = answerB;
         this.answerC = answerC;
         this.answerD = answerD;
-
+        answers.add(answerA);
+        answers.add(answerB);
+        answers.add(answerC);
+        answers.add(answerD);
+        Collections.shuffle(answers);
     }
+
+    public int getUid() {
+        return uid;
+    }
+
+    public void setUid(int uid) {
+        this.uid = uid;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public String getAnswerA() {
+        return answerA;
+    }
+
+    public void setAnswerA(String answerA) {
+        this.answerA = answerA;
+    }
+
+    public String getAnswerB() {
+        return answerB;
+    }
+
+    public void setAnswerB(String answerB) {
+        this.answerB = answerB;
+    }
+
+    public String getAnswerC() {
+        return answerC;
+    }
+
+    public void setAnswerC(String answerC) {
+        this.answerC = answerC;
+    }
+
+    public String getAnswerD() {
+        return answerD;
+    }
+
+    public void setAnswerD(String answerD) {
+        this.answerD = answerD;
+    }
+
+    public ArrayList<String> getAnswers() {
+        return answers;
+    }
+
+
 }
